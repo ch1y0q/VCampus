4d3654e6c89728944b54c5b475676500e8ac0f08
qwerty250 <qiyue2001@gmail.com>
2021-07-06 19:57:22 +0800

    不带你

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,5 @@ import java.io.*;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.println("qydlddw");
     }
 }
