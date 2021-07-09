4fee6dce394adfe4a209724881e5f6c459b90476
opcoast <56123967+opcoast@users.noreply.github.com>
2021-07-09 16:41:47 +0800

    ADD net.Request

---
+++ src/com/vcampus/net/ConnectionToServer.java
@@ -0,0 +1,35 @@
+package com.vcampus.net;
+
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * 客户端到服务端的连接类
+ *
+ * @author Franklin Yang
+ * @date 2021/7/9
+ */
+public class ConnectionToServer extends Connection{
+    public ConnectionToServer(Socket socket) {
+        super(socket);
+    }
+
+    @Override
+    public void write(String content) {
+        synchronized (ConnectionToServer.class) {
+            this.pWriter.write(content + "\n");
+            this.pWriter.flush();
+        }
+    }
+
+    @Override
+    public synchronized String readLine() {
+        // readLine本身就是阻塞的，无需同步
+        try {
+            return this.bReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
