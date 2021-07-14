9eecdd66423282ff519a4d6487bc04732dc69f1f
qwerty250 <qiyue2001@gmail.com>
2021-07-14 16:36:00 +0800

    ADD some translations

--- src/com/vcampus/dao/IAdminMapper.java
+++ src/com/vcampus/dao/IAdminMapper.java
@@ -7,7 +7,7 @@ import com.vcampus.entity.Admin;
  * @date 2021/7/14
  */
 public interface IAdminMapper {
-    public Boolean verifyManager(Admin admin);
+    public Boolean verifyAdmin(Admin admin);
 
     public Admin getAdminDetailByCardNumber(String cardNumber);
 }
