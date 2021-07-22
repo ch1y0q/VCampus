0ef3122c49e960370293d14861122d129178f385
meteoseeker <drj2364082@163.com>
2021-07-23 01:48:35 +0800

    个人信息界面

---
+++ src/com/vcampus/test/AppStuInfoEntry.java
@@ -0,0 +1,22 @@
+package com.vcampus.test;
+
+import com.alee.laf.WebLookAndFeel;
+import com.vcampus.client.administrator.main.AppAdminInfo;
+import com.vcampus.client.main.AppStuInfo;
+
+/**
+ * 学生信息界面测试
+ * @author Dong Ruojing
+ * @date 2021/7/23
+ */
+public class AppStuInfoEntry {
+    public static void main(String[]args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                WebLookAndFeel.install ();
+                AppStuInfo app = new AppStuInfo();
+                app.setVisible(true);
+            }
+        });
+    }
+}
---
+++ src/com/vcampus/test/AppTeaInfoEntry.java
@@ -0,0 +1,22 @@
+package com.vcampus.test;
+
+import com.alee.laf.WebLookAndFeel;
+import com.vcampus.client.main.AppStuInfo;
+import com.vcampus.client.main.AppTeaInfo;
+
+/**
+ * 教师信息界面测试
+ * @author Dong Ruojing
+ * @date 2021/7/23
+ */
+public class AppTeaInfoEntry {
+    public static void main(String[]args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                WebLookAndFeel.install ();
+                AppTeaInfo app = new AppTeaInfo();
+                app.setVisible(true);
+            }
+        });
+    }
+}
