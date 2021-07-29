5951f7a0abf9542d5f3bb417b5494394b3e8e571
meteoseeker <drj2364082@163.com>
2021-07-29 16:47:20 +0800

    管理员接受每日上报基本实现，筛选功能有bug

--- src/com/vcampus/client/main/dailyReport/DailyReportManagePanel.java
+++ src/com/vcampus/client/main/dailyReport/DailyReportManagePanel.java
@@ -1,9 +1,25 @@
 package com.vcampus.client.main.dailyReport;
 
+import com.alee.managers.style.StyleId;
+import com.vcampus.client.main.App;
+import com.vcampus.dao.IStudentMapper;
+import com.vcampus.entity.PersonWhoReport;
+import com.vcampus.entity.Student;
+import com.vcampus.net.Request;
+import com.vcampus.net.Response;
+import com.vcampus.util.ResponseUtils;
+import org.apache.ibatis.session.SqlSession;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * 这是管理员接受每日上报的panel，用于目录的整合
@@ -13,11 +29,13 @@ import java.awt.*;
 public class DailyReportManagePanel extends JPanel {
     private int width = 1151;
     private int height = 800;
-    DailyReportManagePanel(){
+    private List<PersonWhoReport> list = null;
+    private DefaultTableModel model;
+    DailyReportManagePanel() {
 
         String[][] emptyTable = {};
         String[] emptyData = {};
-        String[] school = {"建筑学院", "机械工程学院", "能源与环境学院", "信息科学与工程学院", "土木工程学院", "电子科学与工程学院",
+        String[] school = {"","建筑学院", "机械工程学院", "能源与环境学院", "信息科学与工程学院", "土木工程学院", "电子科学与工程学院",
                 "数学学院", "自动化学院", "计算机科学与工程学院", "物理学院", "生物科学与医学工程学院", "材料科学与工程学院", "人文学院",
                 "经济管理学院", "马克思主义学院", "电气工程学院", "外国语学院", "化学化工学院", "交通学院", "仪器科学与工程学院", "艺术学院",
                 "法学院", "医学院1", "公共卫生学院", "医学院2", "网络空间安全学院", "人工智能学院", "吴健雄学院", "软件学院"};
@@ -32,15 +50,16 @@ public class DailyReportManagePanel extends JPanel {
         setLayout(null);
 
         JScrollPane sp = new JScrollPane();
-        sp.setBounds(width/50, 30+height/20, 1100, height * 3 / 5);
+        sp.setBounds(width / 50, 30 + height / 20, 1100, height * 3 / 5);
         sp.setBorder(BorderFactory.createLineBorder(Color.red, 1));
         sp.setBackground(Color.white);
         add(sp);
-        String[] columnName={"一卡通号","姓名","学院","当日晨检温度","目前所在位置","目前所在校区",
-                "是否在隔离期","是否为疑似病例","是否为确诊病例","有无风险区旅居史"};
-        DefaultTableModel model = new DefaultTableModel(emptyTable,columnName);
-        JTable Table = new JTable(model){
-            public boolean isCellEditable(int row, int column){
+        String[] columnName = { "一卡通号", "姓名", "学院","日期", "当日晨检温度", "目前所在位置","所在城市", "目前所在校区",
+                "是否在隔离期", "是否为疑似病例", "是否为确诊病例", "有无风险区旅居史"};
+        model = new DefaultTableModel(emptyTable, columnName);
+        JTable Table = new JTable(model) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
                 this.setRowSelectionAllowed(false);
                 this.setColumnSelectionAllowed(false);
                 return false;
@@ -50,44 +69,137 @@ public class DailyReportManagePanel extends JPanel {
         for (int i = 0; i < 5; i++) {
             model.addRow(emptyData);
         }
-        JLabel schoolLabel = new JLabel("学院",JLabel.CENTER);
-        schoolLabel.setBounds(width/50,height/40,40,30);
+        JLabel schoolLabel = new JLabel("学院", JLabel.CENTER);
+        schoolLabel.setBounds(width / 50, height / 40, 40, 30);
         add(schoolLabel);
 
         JComboBox chooseSchool = new JComboBox();
         for (String s : school) {
             chooseSchool.addItem(s);
         }
-        chooseSchool.setBounds(width/50+60,height/40,120,30);
+        chooseSchool.setBounds(width / 50 + 60, height / 40, 120, 30);
         add(chooseSchool);
 
-        JLabel numNotReport = new JLabel("未上报人数",JLabel.CENTER);
-        numNotReport.setBounds(width/25+240,height/40,100,30);
+        JLabel numNotReport = new JLabel("未上报人数", JLabel.CENTER);
+        numNotReport.setBounds(width / 25 + 240, height / 40, 100, 30);
         add(numNotReport);
 
         JLabel lblNumNotReport = new JLabel();
-        lblNumNotReport.setBounds(width/25+350,height/40,80,30);
+        lblNumNotReport.setBounds(width / 25 + 350, height / 40, 80, 30);
         lblNumNotReport.setFont(new Font("微软雅黑", Font.BOLD, 14));
         lblNumNotReport.setOpaque(true);
         lblNumNotReport.setForeground(new Color(33, 117, 206));
-        lblNumNotReport.setBackground(new Color(33,177,206,80));
+        lblNumNotReport.setBackground(new Color(33, 177, 206, 80));
         lblNumNotReport.setHorizontalAlignment(SwingConstants.CENTER);
         add(lblNumNotReport);
 
-        JLabel numAbnormal = new JLabel("体温>37.3℃的人数",JLabel.CENTER);
-        numAbnormal.setBounds(width*2/25+450,height/40,150,30);
+        JLabel numAbnormal = new JLabel("体温>37.3℃的人数", JLabel.CENTER);
+        numAbnormal.setBounds(width * 2 / 25 + 450, height / 40, 150, 30);
         add(numAbnormal);
 
         JLabel lblNumAbnormal = new JLabel();
-        lblNumAbnormal.setBounds(width*2/25+610,height/40,80,30);
+        lblNumAbnormal.setBounds(width * 2 / 25 + 610, height / 40, 80, 30);
         lblNumAbnormal.setFont(new Font("微软雅黑", Font.BOLD, 14));
         lblNumAbnormal.setOpaque(true);
         lblNumAbnormal.setForeground(new Color(33, 117, 206));
-        lblNumAbnormal.setBackground(new Color(33,177,206,80));
+        lblNumAbnormal.setBackground(new Color(33, 177, 206, 80));
         lblNumAbnormal.setHorizontalAlignment(SwingConstants.CENTER);
-        lblNumAbnormal.setText("24");
+        lblNumAbnormal.setText("5");
         add(lblNumAbnormal);
+
+
+        //刷新接受信息
+        final JButton btnRenew = new JButton("刷新");
+        btnRenew.putClientProperty(StyleId.STYLE_PROPERTY, StyleId.buttonHover);
+        btnRenew.setBounds(width * 2 / 25 + 800, height / 40, 80, 30);
+        btnRenew.setOpaque(true);
+        btnRenew.setFont(new Font("微软雅黑", Font.BOLD, 18));
+        btnRenew.setForeground(new Color(0xD5B8B8));
+        btnRenew.setBackground(new Color(0xDA61A4B8, true));
+        btnRenew.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2,
+                new Color(255, 255, 255, 255)));
+        btnRenew.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == btnRenew) {
+                    String[][] listData;
+                    //是否选择学院
+                    if(chooseSchool.getSelectedItem().toString()!="") {//显示全部
+                        System.out.println("llll");
+
+                        renewBySchool(chooseSchool.getSelectedItem().toString());
+                        listData = new String[list.size()][12];
+
+                    }
+                    else {
+                        renew();listData = new String[list.size()][12];
+                        System.out.println("筛选");
+                    }
+
+                    if (list == null || list.size() == 0) {
+                        System.out.println("error");
+                        model.setRowCount(0);
+                        Table.setModel(model);
+                    } else {
+                        model.setRowCount(0);
+                        int len = list.size();
+                        for (int i = 0; i < len; i++) {
+                            listData[i][0]=list.get(i).getCardNumber();
+                            listData[i][1]=list.get(i).getName();
+                            listData[i][2]=list.get(i).getSchool();
+                            listData[i][3]=list.get(i).getDate().toString();
+                            String temp=String.valueOf(list.get(i).getTemperature());
+                            listData[i][4]=temp;
+                            listData[i][5]=list.get(i).getLocation();
+                            listData[i][6]=list.get(i).getCity();
+                            listData[i][7]=list.get(i).getCampus();
+                            listData[i][8]=list.get(i).getIfQarantined();
+                            listData[i][9]=list.get(i).getIfSuspected();
+                            listData[i][10]=list.get(i).getIfDefinite();
+                            listData[i][11]=list.get(i).getIfHistoryOfRiskyArea();
+
+                        }
+                        model = new DefaultTableModel(listData, columnName){
+                            public boolean isCellEditable(int a, int b) {
+                                return false;
+                            }
+                        };
+                        Table.setModel(model);
+                    }
+
+                    JOptionPane.showMessageDialog(null, "刷新成功");
+
+                }
+            }
+        });
+        add(btnRenew);
+    }
+
+    public void renewBySchool(String chooseSchool) {
+        list = ResponseUtils.getResponseByHash(new Request(App.connectionToServer, null,
+                "com.vcampus.server.dailyReport.DailyReportServer.tableDisplayBySchool",
+                new Object[]{chooseSchool}).send()).getListReturn(PersonWhoReport.class);
+
+    }
+    public void renew(){
+        list = ResponseUtils.getResponseByHash(new Request(App.connectionToServer, null,
+                "com.vcampus.server.dailyReport.DailyReportServer.tableDisplay",
+                new Object[] {}).send()).getListReturn(PersonWhoReport.class);
+        if (list == null || list.size() == 0) {
+            System.out.println("error");
+        }
     }
 
 
+
+
+
 }
+
+
+
+
+
+
+
+
--- src/com/vcampus/server/dailyReport/DailyReportServer.java
+++ src/com/vcampus/server/dailyReport/DailyReportServer.java
@@ -7,6 +7,10 @@ import com.vcampus.entity.Student;
 import com.vcampus.server.App;
 import org.apache.ibatis.session.SqlSession;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * @author Dong Ruojing
  * @date 2021/7/28
@@ -28,5 +32,40 @@ public class DailyReportServer {
             e.printStackTrace();
         }
         return result;
+    }
+
+
+    //根据学院显示表格
+    public static java.util.List<PersonWhoReport> tableDisplayBySchool(String school) {
+        List<PersonWhoReport> result = null;
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = App.sqlSessionFactory.openSession();
+            IPersonWhoReportMapper personWhoReportMapper = sqlSession.getMapper(IPersonWhoReportMapper.class);
+            result = personWhoReportMapper.tableDisplayBySchool(school);
+            sqlSession.commit();
+            sqlSession.close();
+        } catch (Exception e) {
+            sqlSession.rollback();
+            e.printStackTrace();
+        }
+        return result;
+    }
+    //显示表格
+    public static List<PersonWhoReport> tableDisplay() {
+        List<PersonWhoReport> list = new ArrayList<>();
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = App.sqlSessionFactory.openSession();
+            IPersonWhoReportMapper personWhoReportMapper = sqlSession.getMapper(IPersonWhoReportMapper.class);
+            list = personWhoReportMapper.tableDisplay();
+            sqlSession.commit();
+            sqlSession.close();
+            return list;
+        } catch (Exception e) {
+            sqlSession.rollback();
+            e.printStackTrace();
+        }
+        return list;
     }
 }
